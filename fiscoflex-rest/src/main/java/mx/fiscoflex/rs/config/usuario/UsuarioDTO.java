3904ea388be7e929c0b4f40a8d493c186cb2012f
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-04 17:28:46 -0600

    Clase UsuarioDTO

---
+++ fiscoflex-rest/src/main/java/mx/fiscoflex/rs/config/usuario/UsuarioDTO.java
@@ -0,0 +1,59 @@
+package mx.fiscoflex.rs.config.usuario;
+
+public class UsuarioDTO {
+
+	private Integer idUsuario;
+	private String nombre;
+	private String email;
+	private String password;
+	private Boolean activo;
+	private Integer idPerfil;
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
+	}
+
+	public Integer getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Integer idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+}
