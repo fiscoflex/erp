e147563468508f705609da25b35b73c57f23089d
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-04 17:31:36 -0600

    Configuracion Autenticacion

---
+++ fiscoflex-rest/src/main/java/mx/fiscoflex/rs/auth/AuthResource.java
@@ -0,0 +1,5 @@
+package mx.fiscoflex.rs.auth;
+
+public class AuthResource {
+
+}
---
+++ fiscoflex-rest/src/main/java/mx/fiscoflex/rs/auth/ConfiguracionConst.java
@@ -0,0 +1,8 @@
+package mx.fiscoflex.rs.auth;
+
+public class ConfiguracionConst {
+
+	public static final String DURACION_TOKENS = "DURACION TOKENS";
+	public static final String DIR_TEMP = "DIR_TEMP";
+	public static final String DIR_DATOS = "DIR_DATOS";
+}
