60302ac83ad599f656dcdd47aacad026eaf7d631
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-04 17:26:18 -0600

    Clase UsuarioEntity

---
+++ fiscoflex-rest/src/main/java/mx/fiscoflex/rs/persistence/UsuarioEntity.java
@@ -0,0 +1,81 @@
+package mx.fiscoflex.rs.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Usuarios")
+public class UsuarioEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IdUsuario")
+	private Integer idUsuario;
+
+	@Column(name = "Nombre")
+	private String nombre;
+
+	@Column(name = "Email")
+	private String email;
+
+	@Column(name = "Password")
+	private String password;
+
+	@Column(name = "Activo")
+	private Boolean activo;
+
+	@Column(name = "IdPerfil")
+	private Integer idPerfil;
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
+	}
+
+	public Integer getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Integer idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+}
