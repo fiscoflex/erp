dac6c862506076495c233f85a62091cfa21757f1
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-08 16:10:48 -0600

    Filtro

---
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/filter/FiscoFlex.java
@@ -0,0 +1,32 @@
+package mx.fiscoflex.web.filter;
+
+public class FiscoFlex {
+
+	private String token;
+	private FiscoFlex fiscoFlex;
+	
+	public String obtenerToken(String username, String password){
+		
+		if(username.equals("admin") && password.equals("admin")){
+			return  "admin123456";
+		}else{
+			return "";
+		}
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	public FiscoFlex getFiscoFlex() {
+		return fiscoFlex;
+	}
+	
+	public void setFiscoFlex(FiscoFlex fiscoFlex) {
+		this.fiscoFlex = fiscoFlex;
+	}
+}
