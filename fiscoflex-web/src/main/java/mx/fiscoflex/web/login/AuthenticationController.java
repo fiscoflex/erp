8934ea1ab1634eff32d1e4a39ca0b9555b01800d
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-10 20:30:06 -0600

    Authentication

---
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/login/AuthenticationController.java
@@ -0,0 +1,24 @@
+package mx.fiscoflex.web.login;
+
+import javax.security.auth.login.LoginException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthenticationController{
+
+	@RequestMapping(value = "/auth",method = RequestMethod.GET)
+	public String index(Model model){
+		return "/Authentication";
+	}
+	
+	@RequestMapping(value = "/authentication",method = RequestMethod.POST)
+	public String Auth(Model model) throws LoginException{
+		throw new LoginException();
+		//return "/Authentication";
+	}
+
+}
