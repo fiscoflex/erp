e558baf89d282d4ddcce92e8ef0b5a74ae54f61d
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-11 18:44:53 -0600

    Security

--- fiscoflex-web/src/main/java/mx/fiscoflex/web/security/AuthenticationSecurity.java
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/security/AuthenticationSecurity.java
@@ -12,47 +12,51 @@ import org.springframework.web.servlet.ModelAndView;
 
 import mx.fiscoflex.web.login.LoginBean;
 
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 
 @ControllerAdvice
 public class AuthenticationSecurity {
+
+	private String ajaxHeaderValue = "XMLHttpRequest";
+	// private String normalHeaderValue = "text/html";
+	private static final String pageLogin = "login";
+
 	/*
-	@ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+	 * @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+	 * 
+	 * @ExceptionHandler(LoginException.class) public void handleConflict() { //
+	 * Nothing to do System.out.println("Error 401"); }
+	 */
+
+	@ResponseStatus(HttpStatus.UNAUTHORIZED) // ERROR 401
 	@ExceptionHandler(LoginException.class)
-	public void handleConflict() {
-	// Nothing to do
-		System.out.println("Error 401");
-	}
-	*/
-	
-	@ExceptionHandler(LoginException.class)
-	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e){
-        // If the exception is annotated with @ResponseStatus rethrow it and let
-        // the framework handle it - like the OrderNotFoundException example
-        // at the start of this post.
-        // AnnotationUtils is a Spring Framework utility class.
-        //if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
-          //  throw e;
-        // Otherwise setup and send the user to a default error-view.
+	public ModelAndView defaultErrorHandler(HttpServletRequest req, LoginException login) throws LoginException {
 		Enumeration<String> headerNames = req.getHeaderNames();
-
 		while (headerNames.hasMoreElements()) {
-
 			String headerName = headerNames.nextElement();
-			System.out.println("HEADERNAME " + headerName);
-
+			// System.out.println("HEADERNAME " + headerName);
 			Enumeration<String> headers = req.getHeaders(headerName);
 			while (headers.hasMoreElements()) {
 				String headerValue = headers.nextElement();
-				System.out.println("HEADERVALUE " + headerValue);
+				// System.out.println("HEADERVALUE " + headerValue);
+				if (headerValue.equals(ajaxHeaderValue)) {
+					System.out.println("Acción ejecutada con Ajax ::");
+					if (AnnotationUtils.findAnnotation(login.getClass(), ResponseStatus.class) != null) {
+						throw login;
+					}
+				} else {
+					// if (headerValue.contains(normalHeaderValue)) {
+					System.out.println("Acción ejecutada normalmente redireccionando::");
+					ModelAndView model = new ModelAndView();
+					LoginBean loginBean = new LoginBean();
+					model.addObject("loginBean", loginBean);
+					model.setViewName(pageLogin);
+					return model;
+					// }
+				}
 			}
-
 		}
-		ModelAndView model = new ModelAndView("login");
-		LoginBean loginBean = new LoginBean();
-		model.addObject("loginBean", loginBean);
-		return model; 
-    }
-
-	
+		return null;
+	}
 }
