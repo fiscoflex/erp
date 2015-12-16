2859a87311c84ede08d76300db65537602a50ce1
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-15 19:30:34 -0600

    LoginController

--- fiscoflex-web/src/main/java/mx/fiscoflex/web/security/LoginController.java
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/security/LoginController.java
@@ -8,50 +8,66 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import mx.fiscoflex.api.CredencialesInvalidasException;
+import mx.fiscoflex.api.Fiscoflex;
+import mx.fiscoflex.api.Usuario;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String login() {
+	public String login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession session = request.getSession();
+		if (session != null) {
+			if (session.getAttribute(ConfiguracionConst.API_NAME) != null) {
+				return "index";
+			}
+		}
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String home(@ModelAttribute(value = "login") LoginBean user, HttpServletResponse response,
-			HttpServletRequest request, Model model) {
-
-		FiscoFlex fiscoflex = new FiscoFlex();
-		String token = fiscoflex.obtenerToken(user.getUsername(), user.getPassword());
-		String checkbox = request.getParameter("checkbox");
-		if (token != null) {
+			HttpServletRequest request, ModelMap model) throws CredencialesInvalidasException{
+		try{
+			Fiscoflex fiscoflex = new Fiscoflex();
+			Usuario usuario = new Usuario();
+			usuario.setNombre(user.getUsername());
+			usuario.setPassword(user.getPassword());
+			
+			String tokenRemoto = fiscoflex.login(usuario);		
+			String checkbox = request.getParameter("checkbox");
 			HttpSession session = request.getSession();
+			
 			session.setAttribute(ConfiguracionConst.API_NAME, fiscoflex);
 			session.setMaxInactiveInterval(30 * 60);
+			
 			if (checkbox != null) {
-				Cookie cookie = new Cookie(ConfiguracionConst.COOKIE_NAME, token);
+				Cookie cookie = new Cookie(ConfiguracionConst.COOKIE_NAME, tokenRemoto);
 				cookie.setMaxAge(30 * 30);
 				response.addCookie(cookie);
 			}
 			return "index";
-		} else {
+		}catch(CredencialesInvalidasException c){
+			model.put("error", "Usuario/Password incorrectos");
 			return "login";
 		}
 	}
 
 	@RequestMapping(value = "logout", method = RequestMethod.GET)
-	public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public String logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		HttpSession session = request.getSession();
 		if (session != null) {
-			System.out.println("Logout");
 			session.invalidate();
 			deleteCookieValue(response, request, ConfiguracionConst.COOKIE_NAME);
+			response.sendRedirect(request.getContextPath() + "/login");
 		}
-		response.sendRedirect(request.getContextPath() + "/login");
+		return "login";
 	}
 
 	public void deleteCookieValue(HttpServletResponse response, HttpServletRequest request, String name) {
@@ -59,7 +75,7 @@ public class LoginController {
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
 				if (cookie != null && name.equals(cookie.getName())) {
-					Cookie invalidCookie = new Cookie(ConfiguracionConst.COOKIE_NAME, "");
+					Cookie invalidCookie = new Cookie(ConfiguracionConst.COOKIE_NAME, null);
 					invalidCookie.setMaxAge(0);
 					response.addCookie(invalidCookie);
 				}
