49ff8a179d015e283f967c68ceffd5720288699e
Luis Alfonso <luisalfonso_pc@hotmail.com>
2015-12-17 19:18:41 -0600

    CuentaContableController

--- fiscoflex-web/src/main/java/mx/fiscoflex/web/contabilidad/CuentaContableController.java
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/contabilidad/CuentaContableController.java
@@ -1,8 +1,15 @@
 package mx.fiscoflex.web.contabilidad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mx.fiscoflex.api.CuentaContable;
+import mx.fiscoflex.api.Fiscoflex;
 
 @Controller
 public class CuentaContableController {
@@ -10,5 +17,14 @@ public class CuentaContableController {
 	@RequestMapping(value = "CuentasContables", method = RequestMethod.GET)
 	public String cuentasContables() {
 		return "CuentasContables";
+	}
+
+	@RequestMapping(value = "cuentas", method = RequestMethod.GET)
+	@ResponseBody
+	public List<CuentaContable> list() {
+		Fiscoflex fisco = new Fiscoflex();
+		List<CuentaContable> lista = new ArrayList<CuentaContable>();
+		lista = fisco.cuentas();
+		return lista;
 	}
 }
