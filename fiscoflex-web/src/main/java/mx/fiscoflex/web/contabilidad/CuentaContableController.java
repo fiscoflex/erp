a5cc879a03371ee0d2a123ead488793612f26cc8
luisalfonso_pc@hotmail.com <luisalfonso_pc@hotmail.com>
2015-12-29 08:54:47 -0600

    Contabilidad

--- fiscoflex-web/src/main/java/mx/fiscoflex/web/contabilidad/CuentaContableController.java
+++ fiscoflex-web/src/main/java/mx/fiscoflex/web/contabilidad/CuentaContableController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import mx.fiscoflex.api.CuentaContable;
@@ -15,6 +16,9 @@ import mx.fiscoflex.api.Fiscoflex;
 
 @Controller
 public class CuentaContableController {
+
+	Fiscoflex fisco = new Fiscoflex();
+	CuentaContable cuenta = new CuentaContable();
 
 	@RequestMapping(value = "CuentasContables", method = RequestMethod.GET)
 	public String cuentasContables() {
@@ -24,7 +28,6 @@ public class CuentaContableController {
 	@RequestMapping(value = "cuentas", method = RequestMethod.GET)
 	@ResponseBody
 	public List<CuentaContable> list() {
-		Fiscoflex fisco = new Fiscoflex();
 		List<CuentaContable> lista = new ArrayList<CuentaContable>();
 		lista = fisco.cuentas();
 		return lista;
@@ -32,28 +35,32 @@ public class CuentaContableController {
 
 	@RequestMapping(value = "crearCuenta", method = RequestMethod.POST)
 	public String crearCuentaContable(HttpServletRequest request) {
-		Fiscoflex fisco = new Fiscoflex();
-		CuentaContable cuenta = new CuentaContable();
 		cuenta.setNombreCuenta(request.getParameter("nombreCuenta"));
 		cuenta.setCuentaPadre(Integer.parseInt(request.getParameter("cuentaPadre")));
 		cuenta.setNaturaleza(request.getParameter("naturaleza"));
 		cuenta.setEstadoFinanciero(request.getParameter("estadoFinanciero"));
-		cuenta.setOrigen(request.getParameter(request.getParameter("origen")));
+		cuenta.setOrigen(request.getParameter("origen"));
 		cuenta.setProfundidad(Integer.parseInt(request.getParameter("profundidad")));
 		fisco.crearCuenta(cuenta);
 		return "CuentasContables";
 	}
 
-	@RequestMapping(value = "editarCuenta", method = RequestMethod.PUT)
+	@RequestMapping(value = "editarCuenta", method = RequestMethod.POST)
 	public String editarCuentaContable(HttpServletRequest request) {
-		Fiscoflex fisco = new Fiscoflex();
-		CuentaContable cuenta = new CuentaContable();
-		cuenta.setNombreCuenta(request.getParameter("nombreCuenta"));
-		cuenta.setNaturaleza(request.getParameter("naturaleza"));
-		cuenta.setEstadoFinanciero(request.getParameter("estadoFinanciero"));
-		cuenta.setOrigen(request.getParameter(request.getParameter("origen")));
+		cuenta.setIdCuentaContable(Integer.parseInt(request.getParameter("idCuentaContableE")));
+		cuenta.setNombreCuenta(request.getParameter("nombreCuentaE"));
+		cuenta.setCuentaPadre(Integer.parseInt(request.getParameter("cuentaPadreE")));
+		cuenta.setNaturaleza(request.getParameter("naturalezaE"));
+		cuenta.setEstadoFinanciero(request.getParameter("estadoFinancieroE"));
+		cuenta.setOrigen(request.getParameter("origenE"));
+		cuenta.setProfundidad(Integer.parseInt(request.getParameter("profundidadE")));
 		fisco.actualizarCuenta(cuenta);
 		return "CuentasContables";
 	}
 
+	@RequestMapping(value = "eliminarCuenta", method = RequestMethod.POST)
+	public void eliminarCuentaContable(@RequestParam String idCuenta) {
+		fisco.eliminarCuenta(Integer.parseInt(idCuenta));
+		//return "CuentasContables";
+	}
 }
